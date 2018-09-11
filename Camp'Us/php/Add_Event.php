509eb4d16e2013f561e5b66e0f7835eb13f961a6
Unknown <leo.arthaud@epsi.fr>
2018-09-11 14:18:43 +0200

    trie&rename

---
+++ Camp'Us/php/Add_Event.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Texte du titre</title>
+    </head>
+    <body>
+        <p>
+        <form method='POST' action='Add_Event_POST.php'>
+            Merci de cocher une activite :<br>
+            <input type="checkbox" name="activite[]" value="Voyager"> Voyager<br>
+            <input type="checkbox" name="activite[]" value="Se loger"> Se loger<br>
+            <input type="checkbox" name="activite[]" value="Se restaurer"> Se restaurer<br>
+            <input type="checkbox" name="activite[]" value="Sortir"> Sortir<br>
+            <input type="checkbox" name="activite[]" value="Se cultiver"> Se cultiver<br>
+            <input type="checkbox" name="activite[]" value="Faire du sport"> Faire du sport<br>
+            <input type="checkbox" name="activite[]" value="Profiter de bon plans "> Profiter de bon plans r<br>
+            <input type="submit" name="activiter" value="Résultats">
+            </p>
+        </form>
+    </body>
+</html>

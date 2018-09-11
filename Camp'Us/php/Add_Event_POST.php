e36a3615ecaf49dbf5dbb76606601cf2a2c96033
Marie-Ange <macha.jumelin@epsi.fr>
2018-09-11 15:47:43 +0200

    bonne nuit petite framboise
    ajout table catégorie + modifs add_event_POST

--- Camp'Us/php/Add_Event_POST.php
+++ Camp'Us/php/Add_Event_POST.php
@@ -1,22 +1,36 @@
 <?php
+
+    session_start();
+// Connexion à la base de données
+    include('Connexion.php');
+
 if(isset($_POST['titre']) AND !empty($_POST['titre']) AND
 isset($_POST['description']) AND !empty($_POST['description']) AND
-isset($_POST['id_launcher']) AND !empty($_POST['id_launcher']) AND
-isset($_POST['categorie']) AND !empty($_POST['categorie']) AND
 isset($_POST['datev']) AND !empty($_POST['datev']) AND
 isset($_POST['heure']) AND !empty($_POST['heure']) AND
 isset($_POST['lieu']) AND !empty($_POST['lieu']) AND
 isset($_POST['description']) AND !empty($_POST['description']))
 {
-    // Connexion à la base de données
-    include('Connexion.php');
-    //TODO Check for duplicate codes
-    //Il me faut le nom des tables de la BD pour pouvoir adapter la requete
-    $req = $bdd->prepare("INSERT INTO Event (titre, id_launcher, categorie, datev, heure, lieu, description) VALUES (?, ?, ?, ?, ?, ?, ?,)");
+
+    //c'est l'insertion de la catégorlie dou délice :oui:
+
+    $id = $req->fetch();
+
+    $js = empty($_POST['voyage']) ? 0 : 1;
+    $php = empty($_POST['loger']) ? 0 : 1;
+    $c = empty($_POST['restauration']) ? 0 : 1;
+    $cpp = empty($_POST['sortir']) ? 0 : 1;
+    $cSharp = empty($_POST['culture']) ? 0 : 1;
+    $html = empty($_POST['sport']) ? 0 : 1;
+    $css = empty($_POST['bonsplans']) ? 0 : 1;
+
+    $req = $bdd->prepare('INSERT INTO categorie (voyage, loger, restauration, sortir, culture, sport, bonsplans) VALUES (?, ?, ?, ?, ?, ?, ?)');
+   
+   $req->execute(array($id[0], $js, $php, $c, $cpp, $cSharp, $html, $css, $sql, $phyton, $java, $oracle, $ruby, $secu, $reseaux, $ipv, $envsys));
+
+    $req = $bdd->prepare("INSERT INTO Event (titre, datev, heure, lieu, description) VALUES (?, ?, ?, ?, ?)");
     $req->execute(array(
         $_POST['titre'],
-        $_POST['id_launcher'],
-        $_POST['categorie'],
         $_POST['datev'],
         $_POST['heure'],
         $_POST['lieu'],

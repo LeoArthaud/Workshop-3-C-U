48042e52d3fb56ff521c06b2134d9832ea1b0138
Unknown <leo.arthaud@epsi.fr>
2018-09-11 08:36:35 +0200

    Ajout fichier add event post

---
+++ Camp'Us/php/Add_Event_POST.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_POST['Sujet']) AND !empty($_POST['Sujet']) AND
+isset($_POST['Contenu']) AND !empty($_POST['Contenu'])
+){
+    // Connexion à la base de données
+    include('connexion.php');
+    //TODO Check for duplicate codes
+    //Il me faut le nom des tables de la BD pour pouvoir adapter la requete
+    $req = $bdd->prepare("INSERT INTO Workshop2_Topic (Sujet, Categorie, Contenu, Id_User_Workshop2_Utilisateur ) VALUES (?, ?, ?,?)");
+    $req->execute(array(
+        $_POST['Sujet'],
+        $_POST['CategoriePage'],
+        $_POST['Contenu'],
+        $_POST['IdUserCurrent']));
+}
+//echo ( $_POST['Sujet']. " ".         $_POST['CategoriePage']. " ".        $_POST['Contenu']. " ".        $_POST['IdUserCurrent']);
+header('Location: '.$_SERVER['HTTP_REFERER']);
+
+?>

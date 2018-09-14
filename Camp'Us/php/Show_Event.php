fe697c4015cddc52a4b1c3172cb26d1362b28445
Marie-Ange <macha.jumelin@epsi.fr>
2018-09-14 14:32:14 +0200

    SHIWEVENT

--- Camp'Us/php/Show_Event.php
+++ Camp'Us/php/Show_Event.php
@@ -192,7 +192,7 @@ require("Connexion.php");
     	echo $_SESSION['prenom'].' '.$_SESSION['nom'];
     ?>
 
-     <form action="Add_Comment_Post.php" method="POST">
+     <form action="Add_Comment_POST.php" method="POST">
 
     <input type="textarea" name="content" id="content" placeholder="Laissez un commentaire..."> <br>
     <input type="submit" value="commentaire">
